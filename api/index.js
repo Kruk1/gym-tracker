49dfc478f36deb1838e8121781f57552ecf0f363
Kruk1 <albertkr@wp.pl>
2022-10-11 23:09:41 +0200

    Change settings

--- api/index.js
+++ api/index.js
@@ -9,10 +9,7 @@ const training = require('./router/training')
 const auth = require('./router/auth')
 const user = require('./router/user')
 const PORT = process.env.PORT || 5000;
-const path = require('path');
 
-
-app.use(express.static(path.resolve(__dirname, '../client/src')));
 app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
@@ -30,9 +27,5 @@ app.use((err, req, res, next) =>
     console.log(err)
     res.status(err.status).json(err)
 })
-
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client/src', 'index.html'));
-  });
 
 app.listen(PORT, () => console.log('Server started on port 5000 '))
