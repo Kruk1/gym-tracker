f4a400e2fb60f4ccab626f29dd45fb7c39489c7c
Kruk1 <albertkr@wp.pl>
2022-10-12 19:31:23 +0200

    Change configuration

--- api/controllers/verifyToken.js
+++ api/controllers/verifyToken.js
@@ -1,13 +1,13 @@
 const jwt = require('jsonwebtoken')
-const {jwttoken} = require('../config.json')
 const MyError = require('../error/error')
+require('dotenv').config()
 
 function verifyToken(req, res, next)
 {
     const token = req.cookies.access_token
     if(!token) throw new MyError('You are not authenticated!', 401, true)
 
-    jwt.verify(token, jwttoken, (err, user) =>
+    jwt.verify(token, process.env.JWTTOKEN, (err, user) =>
     {
         if(err) throw new MyError('Token is not valid! Try login again!', 403, true)
         req.user = user
