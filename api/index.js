89d80c6726c305be53713bd8e78d257c6e3eb4db
Kruk1 <albertkr@wp.pl>
2022-10-31 16:09:39 +0100

    Add heroku awake

--- api/index.js
+++ api/index.js
@@ -10,6 +10,7 @@ const auth = require('./router/auth')
 const user = require('./router/user')
 const PORT = process.env.PORT || 5000
 const path = require('path')
+const herokuAwake = require("heroku-awake");
 
 app.use(cors())
 app.use(express.urlencoded({ extended: true }))
@@ -35,4 +36,8 @@ app.use((err, req, res, next) =>
     res.status(err.status).json(err)
 })
 
-app.listen(PORT, () => console.log('Server started on port 5000 '))
+app.listen(PORT, () => 
+  {
+    herokuAwake('https://gym-tracker-exercise.herokuapp.com')
+    console.log('Server started on port 5000 ')
+  })
